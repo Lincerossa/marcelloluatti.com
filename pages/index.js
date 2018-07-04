e288f0e8a80644e13626e4e34b8e447ae777713b
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-04 14:37:03 +0200

    prova link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
 
 
-export default () => <div>homepage</div>
+export default () => <div>
+  homepage
+  <Link href="/altro">vai in altro</Link>
+</div>
