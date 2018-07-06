d0948d8c7f45f978937eafcdfa239d791ce0d731
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-06 23:47:19 +0200

    [pulizia]

--- pages/post/style.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-
-export const PostWrapper = styled.div`
-  border: 1px solid red;
-`
-
--- pages/post/utility.js
+++
@@ -1,9 +0,0 @@
-import posts from '../../data/posts'
-
-export const getPost = ({ slug }) => {
-  return posts.find(post => post.slug === slug)
-}
-
-export const getSlugFromProps = (props) => {
-  return props && props.url && props.url.query && props.url.query.slug
-}
