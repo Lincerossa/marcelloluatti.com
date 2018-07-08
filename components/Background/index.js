77194690d08042505834d020d7fd441d0e10262e
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-08 15:54:47 +0200

    [components] Background nuova props per gestione colore testo componenti

--- components/Background/index.js
+++ components/Background/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import * as S from './styles'
 
-const Background = ({ color, children }) => (
+const Background = ({ color, textColor, children }) => (
   <S.Background
     color={color}
+    textColor={textColor}
   >
     {children}
   </S.Background>
--- components/Background/styles.js
+++ components/Background/styles.js
@@ -3,4 +3,7 @@ import styled from 'styled-components'
 
 export const Background = styled.div`
   background-color: ${props => props.color};
+  * {
+    color: ${props => props.textColor};
+  }
 `
