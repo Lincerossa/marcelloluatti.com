bb2fa70ea9a99c365c2a29633ded13618eeb71fb
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-09 09:35:13 +0200

    [frontend] post

--- pages/post/index.js
+++ pages/post/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import * as S from './styles'
 import posts from '../../data/posts'
 
+import { List, Wrapper, Padder, Background, Card } from '../../components'
+import Layout from '../../layout'
+
 const getPost = ({ slug }) => {
   return posts.find(post => post.slug === slug)
 }
@@ -16,12 +19,30 @@ export default (props) => {
 
   if(!post) return <div>nessun post corrisponde allo slug</div>
 
-  const { title, content } = post
+  const { supertitle, title, subtitle, media, content } = post
+
+  console.log({post})
   return (
-    <S.PostWrapper>
-      { title && <h1>{title}</h1>}
-      { content && <p>{content}</p>}
-    </S.PostWrapper>
+    <Layout>
+      <Background color="#e2473b" textColor="#fff">
+        {media && media.url && <S.MediaWrapper>
+          <S.Media src={media.url} />
+          <Wrapper>
+          <S.MediaHeader>
+            <S.Supertitle>{supertitle}</S.Supertitle>
+            <S.Title>{title}</S.Title>
+            <S.Subtitle>{subtitle}</S.Subtitle>
+          </S.MediaHeader>
+          </Wrapper>
+
+        </S.MediaWrapper>}
+        <Wrapper>
+          <Padder size="big">
+            { content && <div>{content}</div>}
+          </Padder>
+        </Wrapper>
+    </Background>
+    </Layout>
   )
 
 }
--- pages/post/styles.js
+++ pages/post/styles.js
@@ -1,7 +1,51 @@
 import styled from 'styled-components'
 
 
-export const PostWrapper = styled.div`
-  border: 1px solid red;
+
+export const MediaWrapper = styled.div`
+  height: calc(100vh - 2.5rem);
+  position: relative;
+  &:before{
+    content: "";
+    position: absolute;
+    background: linear-gradient(to top,rgba(0,0,0,.75),transparent);
+    bottom: 0;
+    right: 0;
+    left: 0;
+    top: 0;
+    background: linear-gradient(to top,rgba(0,0,0,.75),transparent);
+  }
 `
 
+export const Media = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center center;
+`
+
+export const MediaHeader = styled.div`
+  position: absolute;
+  bottom: 3rem;
+`
+
+
+export const Supertitle = styled.div`
+  font-family: sans-serif;
+  color: #e74f30;
+  font-size: 1.25rem;
+  text-transform: uppercase;
+  margin-bottom: 1.5rem;
+`
+
+export const Title = styled.div`
+  font-size: 3rem;
+  font-weight: 600;
+  line-height: 1;
+  margin-bottom: 1rem;
+`
+
+export const Subtitle = styled.div`
+  text-transform: uppercase;
+`
