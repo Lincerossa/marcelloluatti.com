a8e06718c84370d626aa183bcbb272869fd910a0
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-10 00:21:05 +0200

    [frontend] project

---
+++ pages/project/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import * as S from './styles'
+import projects from '../../data/projects'
+
+import { Wrapper, Padder, Background } from '../../components'
+import Layout from '../../layout'
+import { getSlugFromProps } from '../utility'
+
+
+export default (props) => {
+  const slug = getSlugFromProps(props)
+  const project = projects.find(project => project.slug === slug)
+
+  if(!project) return <div>nessun project corrisponde allo slug</div>
+
+  const { supertitle, title, subtitle, media, content } = project
+
+  return (
+    <Layout>
+      <Background color="#e2473b" textColor="#fff">
+        {media && media.url && <S.MediaWrapper>
+          <S.Media src={media.url} />
+          <Wrapper>
+          <S.MediaHeader>
+            <S.Supertitle>{supertitle}</S.Supertitle>
+            <S.Title>{title}</S.Title>
+            <S.Subtitle>{subtitle}</S.Subtitle>
+          </S.MediaHeader>
+          </Wrapper>
+
+        </S.MediaWrapper>}
+        <Wrapper>
+          <Padder size="big">
+            { content && <div>{content}</div>}
+          </Padder>
+        </Wrapper>
+    </Background>
+    </Layout>
+  )
+
+}
