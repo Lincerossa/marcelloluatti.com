4fe63e9c7ac45386d62712c6c3e09f1eb15285eb
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-10 00:21:41 +0200

    [model] aggiunti dati progetti

---
+++ data/projects/index.js
@@ -0,0 +1,14 @@
+const moleskine = require('./moleskine')
+const fondoambiente = require('./fondoambiente')
+const corneliani = require('./corneliani')
+const salini = require('./salini')
+
+
+const projects = [
+  moleskine,
+  fondoambiente,
+  corneliani,
+  salini
+]
+
+module.exports = projects
