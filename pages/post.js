710f1cb8fdf115af154192422c416f15c5a31694
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-12 23:11:21 +0200

    [pulizia] bella rimozione di codice inutile

--- pages/post.js
+++ pages/post.js
@@ -1,95 +1,33 @@
 import React from 'react'
-import posts from '../data/posts'
 
-import { Wrapper, Padder, Background } from '../components'
+import { Wrapper, Padder, Background, PostHero } from '../components'
 import { getSlugFromProps } from '../utility'
 import globalProvider from '../hoc'
-import styled from 'styled-components'
 
-const Page = (props) => {
+const Page = ({ url }) => {
 
-  const slug = getSlugFromProps(props)
-  const post = posts.find(post => post.slug === slug)
-
-  if(!post) return <div>nessun post corrisponde allo slug</div>
-
-  const { supertitle, title, subtitle, media, content } = post
+  const { query } = url
+  const { supertitle, title, subtitle, media, content } = query
 
   return (
-    <Background color="#e2473b" textColor="#fff">
-      {media && media.url && <MediaWrapper>
-        <Media src={media.url} />
+    <React.Fragment>
+      <PostHero 
+        media={media}
+        supertitle={supertitle}
+        title={title}
+        subtitle={subtitle}
+      />
+      <Background color="#e2473b" textColor="#fff">
         <Wrapper>
-        <MediaHeader>
-          <Supertitle>{supertitle}</Supertitle>
-          <Title>{title}</Title>
-          <Subtitle>{subtitle}</Subtitle>
-        </MediaHeader>
+          <Padder size="big">
+            { content && <div>{content}</div>}
+          </Padder>
         </Wrapper>
-
-      </MediaWrapper>}
-      <Wrapper>
-        <Padder size="big">
-          { content && <div>{content}</div>}
-        </Padder>
-      </Wrapper>
-    </Background>
+      </Background>
+    </React.Fragment>
   )
-
 }
 
-
-
-
-
-
-const MediaWrapper = styled.div`
-  height: calc(100vh - 2.5rem);
-  position: relative;
-  &:before{
-    content: "";
-    position: absolute;
-    background: linear-gradient(to top,rgba(0,0,0,.75),transparent);
-    bottom: 0;
-    right: 0;
-    left: 0;
-    top: 0;
-    background: linear-gradient(to top,rgba(0,0,0,.75),transparent);
-  }
-`
-
-const Media = styled.img`
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center center;
-`
-
-const MediaHeader = styled.div`
-  position: absolute;
-  bottom: 3rem;
-`
-
-
-const Supertitle = styled.div`
-  font-family: sans-serif;
-  color: ${ props => props.theme.colors.orange};
-  font-size: 1.25rem;
-  text-transform: uppercase;
-  margin-bottom: 1.5rem;
-`
-
-const Title = styled.div`
-  font-size: 3rem;
-  font-weight: 600;
-  line-height: 1;
-  margin-bottom: 1rem;
-`
-
-const Subtitle = styled.div`
-  text-transform: uppercase;
-`
 
 
 export default globalProvider(Page)
--- utility/index.js
+++
@@ -1,3 +0,0 @@
-export const getSlugFromProps = (props) => {
-  return props && props.url.query && props.url.query.slug
-}
