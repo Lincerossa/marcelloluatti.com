c3615193e45508f6619ccd1125cbc352b2b42ae5
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-14 14:57:08 +0200

    [frontend] _document: in getInitialProps (lato server) ricevo proprietà della pagina da renderizzare. Posso così dinamicizzare alcuni meta tag  (title & description per ora)

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,30 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
+  static getInitialProps (ctx) {
+
+    const seo = (ctx.query && ctx.query.seo) || {
+      title: 'Marcello Luatti',
+      description: 'Hi, I am Marcello Luatti',
+    }
+
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = ctx.renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
+    return { ...page, styleTags, seo }
   }
   render () {
+
+    const { styleTags, seo } = this.props
     return (
       <html lang="it">
         <Head>
-          <title>Marcello Luatti</title>
+          <title>{seo.title}</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content={seo.description} />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
-          {this.props.styleTags}
+          {styleTags}
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121897228-1"></script>
           <script dangerouslySetInnerHTML={{__html: ` window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
@@ -22,15 +32,14 @@ export default class MyDocument extends Document {
             gtag('config', 'UA-121897228-1')`}} 
           />
           <script dangerouslySetInnerHTML={{__html: `
-              (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:940589,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:940589,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}} />
         </Head>
         <body>
@@ -41,4 +50,3 @@ export default class MyDocument extends Document {
     )
   }
 }
-
