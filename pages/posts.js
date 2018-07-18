a0fe13a5a7211e08786c7d0e72ba1cd21d6847ce
Lincerossa90 <marcello.luatti@alkemy.com>
2018-07-18 14:48:59 +0200

    [frontend] pages / posts: utilizzo FiltersBar

--- pages/posts.js
+++ pages/posts.js
@@ -1,51 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
-import styled from 'styled-components'
 
 import theme from '../styles/theme'
-import { List, Wrapper, Padder, Card, TextBlock, Background, Loading } from '../components'
+import { List, Wrapper, Padder, Card, TextBlock, Background, FiltersBar } from '../components'
 import { DataConsumer } from '../hoc/withDataProvider'
 import globalProvider from '../hoc'
-
-const Input = (props) => (
-  <InputWrapper>
-    <input {...props} />
-    {props.loading && <LoadingWrapper><Loading /></LoadingWrapper>}
-  </InputWrapper>
-)
-
-
-const InputWrapper = styled.div`
-  position: relative;
-  max-width: 300px;
-  input {
-    width: 100%;
-    margin: 0;
-    font-size: 1.25rem;
-    letter-spacing: .04em;
-    padding: .25rem;
-    font-family: sans-serif;
-  }
-`
-
-const LoadingWrapper = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-
-`
-
-
-
 
 
 const Page = () => (
   <>
     <TextBlock color={{text: 'white', background:theme.colors.dark}}>
       <h1>Posts</h1> 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mi magna, blandit vel mauris id, venenatis euismod dolor. Nullam orci erat, tincidunt at dapibus eget, iaculis ut massa. Praesent ut justo quis ligula volutpat lacinia. Integer ac pulvinar nibh, et feugiat tortor. Fusce vehicula eleifend quam. Vivamus leo lectus, tristique vitae augue eu, faucibus lobortis magna. Nulla facilisi.
-
-Vestibulum et feugiat ex. Maecenas libero tortor, aliquet id condimentum nec, lobortis sed dolor. Nunc nec erat non odio dignissim tempor. Maecenas et ligula vitae enim lacinia mattis porta vitae ex. Mauris et congue dolor. Aenean id odio venenatis, condimentum nibh eu, tempus urna. Aliquam venenatis lacus venenatis malesuada tincidunt. Phasellus porttitor facilisis consequat. Quisque commodo massa augue, eget eleifend augue commodo id.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mi magna, blandit vel mauris id, venenatis euismod dolor. Nullam orci erat, tincidunt at dapibus eget, iaculis ut massa. Praesent ut justo quis ligula volutpat lacinia. Integer ac pulvinar nibh, et feugiat tortor. Fusce vehicula eleifend quam. Vivamus leo lectus, tristique vitae augue eu, faucibus lobortis magna. Nulla facilisi.</p>
     </TextBlock>
     <Background color={{background:theme.colors.lightGrey}}>
       <Wrapper size="medium">
@@ -55,7 +21,7 @@ Vestibulum et feugiat ex. Maecenas libero tortor, aliquet id condimentum nec, lo
               ({ posts, loading, handleSearch}) => {
                   return(
                     <>
-                      <Input
+                      <FiltersBar
                         placeholder="Cerca un post" 
                         onChange={e => handleSearch({query: e.target.value, data: 'posts'})}
                         loading={loading}
