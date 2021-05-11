52c2babbe6ad18efb868df5a3a0fe738240a985c
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-11 16:09:01 +0200

    🔧 cloudinary configured as trusted domains

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withTM = require('next-transpile-modules')(['three'])
-module.exports = withTM()
+module.exports = {
+  ...withTM(),
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+}
