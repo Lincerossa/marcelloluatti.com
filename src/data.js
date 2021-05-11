2badf8be0fe867a2b640a7f09d56b59b661acd70
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-11 11:30:52 +0200

    ✏️ data changed a bit the structure (declared the component key in each component)

--- src/data.js
+++ src/data.js
@@ -3,17 +3,41 @@ const data = {
     {
       slug: "",
       label: "Marcello Luatti",
-      metaTitle: "Marcello Luatti | dev",
+      metaTitle: "Marcello Luatti | CavLince",
       metaDescription: "Marcello Luatti description",
       metaImage: null,
       hidden: true,
       componentIds: ["RichText_homepage"]
     },
+    {
+      slug: "due",
+      label: "due",
+      metaTitle: "Marcello Luatti | CavLince",
+      metaDescription: "Marcello Luatti description",
+      metaImage: null,
+      componentIds: ["RichText_homepage"]
+    },
+    {
+      slug: "due/tre",
+      label: "tre",
+      metaTitle: "Marcello Luatti | CavLince",
+      metaDescription: "Marcello Luatti description",
+      metaImage: null,
+      componentIds: ["RichText_homepage2"]
+    },
   ],
   components: [
     {
       id: "RichText_homepage",
-      text: "Marcello Luatti",
+      component: "RichText",
+      text: "Prova",
+      background: "red",
+      padder: "large",
+    },
+    {
+      id: "RichText_homepage2",
+      component: "RichText",
+      text: "Prova222",
       background: "red",
       padder: "large",
     },
