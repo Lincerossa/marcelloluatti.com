19d0610d5457f5ac55895292e94be3dad944b1df
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-11 10:22:22 +0200

    🔥 removed storybook and everything connected to it

--- components/Wrapper/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import Wrapper from './index'
-
-export default () =>
-  storiesOf('Wrapper', module)
-    .add('Default', () => <Wrapper size="small">Prova wrapping small</Wrapper>)
-    .add('Wrapper medium', () => <Wrapper size="medium">Prova wrapper medium</Wrapper>)
