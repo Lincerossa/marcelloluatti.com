77e22cf1136cb8f912c0fb9744c47ebd8a67dd47
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-11 14:21:42 +0200

    💅🏻 added Title styles

--- src/components/R3F/styles.js
+++ src/components/R3F/styles.js
@@ -6,3 +6,15 @@ export const CanvasWrapper = styled.div`
 
   }
 `
+export const Title = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  left: 2rem;
+  z-index: 2;
+  font-size: 3rem;
+  color: white;
+  span {
+    font-size: 3rem;
+    color: ${props => props.theme.colors.primary};
+  }
+`
