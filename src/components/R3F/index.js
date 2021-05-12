f9604519f99f81b59335f65451fc5236002e5b62
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-12 16:53:13 +0200

    🔧 R3F component: used meshNormalMaterial (the face color depens on the normal of the geometry surface. nice colour!) and Sky is from drei.

--- src/components/R3F/index.js
+++ src/components/R3F/index.js
@@ -1,8 +1,7 @@
 import React, {Suspense, useCallback, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Canvas, useFrame } from '@react-three/fiber'
-import ArrowDownOutlined from '@ant-design/icons/ArrowDownOutlined'
-import { Environment, OrbitControls, useGLTF, Text } from "@react-three/drei";
+import { OrbitControls, useGLTF, Text, Stars } from "@react-three/drei";
 import * as S from './styles'
 import theme from '../../styles/theme';
 
@@ -14,19 +13,16 @@ const Model = (props) => {
   const { materials } = useGLTF("/test.gltf");
 
   useFrame(({clock}) => {
-    const time = clock.getElapsedTime() / 2.5
+    const time = clock.getElapsedTime() / 2
     if(time > props.timer) setActive(true)
     group.current.rotation.y = props.negative ? + time : - time
   })
 
   return (
     <group >
-      <group ref={group} {...props} dispose={null} scale={0.4} position={props.position}  >
-        <mesh
-          castShadow
-          receiveShadow
-          material={materials["Material.001"]}
-        >
+      <group ref={group} {...props} dispose={null} scale={0.4} position={props.position}>
+        <mesh>
+          <meshNormalMaterial  />
           <boxGeometry args={[1,2,1]} />
         </mesh>
       </group>
@@ -35,8 +31,7 @@ const Model = (props) => {
           fontSize={.3}
           cursor="pointer"
           color={hovered ? theme.colors.primary : "white"}
-          anchorX="center"
-          anchorY="middle"
+          textAlign="left"
           onClick={props.onClick}
           onPointerEnter={() => setHovered(true)}
           onPointerLeave={() => setHovered(false)} 
@@ -49,32 +44,21 @@ const Model = (props) => {
 }
 const R3F = () => {
   const router = useRouter()
-  function handleScrollDown (){
-    if(typeof window !== "undefined") {
-      window.scroll({
-        top: window.innerHeight,
-        left: 0,
-        behavior: 'smooth'
-      });
-    }
-  }
+  
+
   return (
     <S.CanvasWrapper>
       <Canvas>
         <Suspense fallback={null}>
+          <OrbitControls />
+          <Stars />
           <Model position={[0, 2, 0]} text="About" timer={.5} negative onClick={() => router.push("/about")} />
           <Model position={[0, 1, 0]} text="Projects" timer={.75} onClick={() => router.push("/projects")} />
           <Model position={[0, 0, 0]} text="Blog" timer={1} negative onClick={() => router.push("/blog")}/>
           <Model position={[0, -1, 0]} text="Lorem" timer={1.25} onClick={() => router.push("/lorem")}/>
           <Model position={[0, -2, 0]} text="Ipsum" timer={1.5} negative onClick={() => router.push("/ipsum")}/>
-          <OrbitControls />
-          <fog attach="fog" args={['white', 50, 190]} />
-          <Environment preset="park" background  />
-          <ambientLight intensity={1} />
-          <directionalLight color={theme.colors.thirdary} position={[0, 1, 3]} />
         </Suspense>
       </Canvas>
-      <S.IconWrapper onClick={handleScrollDown}><ArrowDownOutlined /></S.IconWrapper>
     </S.CanvasWrapper>
   )
 }
--- src/components/R3F/styles.js
+++ src/components/R3F/styles.js
@@ -7,23 +7,3 @@ export const CanvasWrapper = styled.div`
     background-color: ${props => props.theme.colors.secondary};
   }
 `
-
-export const IconWrapper = styled.div`
-  position: absolute;
-  bottom: 1rem;
-  z-index: 2;
-  color: white;
-  cursor: pointer;
-  left: 50%;
-  transform: translate(-50%, 0);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:hover{
-    color: ${props => props.theme.colors.primary};
-  }
-  svg {
-    font-size: 2rem;
-  }
-`
