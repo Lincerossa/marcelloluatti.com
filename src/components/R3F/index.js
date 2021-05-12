3e4e4be216bef02cec351c1b4d3de49d2c0f6476
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-12 12:08:36 +0200

    🚀 R3F: temporary useRuoter + R3F

--- src/components/R3F/index.js
+++ src/components/R3F/index.js
@@ -1,4 +1,5 @@
 import React, {Suspense, useCallback, useRef, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Canvas, useFrame } from '@react-three/fiber'
 import ArrowDownOutlined from '@ant-design/icons/ArrowDownOutlined'
 import { Environment, OrbitControls, useGLTF, Text } from "@react-three/drei";
@@ -7,17 +8,19 @@ import theme from '../../styles/theme';
 
 const Model = (props) => {
   const [isActive, setActive] = useState(null)
+  const [hovered, setHovered] = useState(null)
   const group = useRef();
 
   const { materials } = useGLTF("/test.gltf");
 
   useFrame(({clock}) => {
     const time = clock.getElapsedTime() / 2.5
+    if(time > props.timer) setActive(true)
     group.current.rotation.y = props.negative ? + time : - time
   })
 
   return (
-    <group onPointerLeave={() => setActive(false)} onPointerEnter={() => setActive(true)}>
+    <group >
       <group ref={group} {...props} dispose={null} scale={0.4} position={props.position}  >
         <mesh
           castShadow
@@ -29,10 +32,14 @@ const Model = (props) => {
       </group>
       {isActive && <group {...props} dispose={null} position={[1, props.position[1], props.position[2]]} >
         <Text
-          fontSize={.25}
-          color="white"
+          fontSize={.3}
+          cursor="pointer"
+          color={hovered ? theme.colors.primary : "white"}
           anchorX="center"
           anchorY="middle"
+          onClick={props.onClick}
+          onPointerEnter={() => setHovered(true)}
+          onPointerLeave={() => setHovered(false)} 
         >
           {props.text}
         </Text>
@@ -41,7 +48,7 @@ const Model = (props) => {
   )
 }
 const R3F = () => {
-
+  const router = useRouter()
   function handleScrollDown (){
     if(typeof window !== "undefined") {
       window.scroll({
@@ -55,11 +62,11 @@ const R3F = () => {
     <S.CanvasWrapper>
       <Canvas>
         <Suspense fallback={null}>
-          <Model position={[0, 2, 0]} text="About" negative/>
-          <Model position={[0, 1, 0]} text="Projects" />
-          <Model position={[0, 0, 0]} text="Blog" negative/>
-          <Model position={[0, -1, 0]} text="Lorem"/>
-          <Model position={[0, -2, 0]} text="Ipsum" negative/>
+          <Model position={[0, 2, 0]} text="About" timer={.5} negative onClick={() => router.push("/about")} />
+          <Model position={[0, 1, 0]} text="Projects" timer={.75} onClick={() => router.push("/projects")} />
+          <Model position={[0, 0, 0]} text="Blog" timer={1} negative onClick={() => router.push("/blog")}/>
+          <Model position={[0, -1, 0]} text="Lorem" timer={1.25} onClick={() => router.push("/lorem")}/>
+          <Model position={[0, -2, 0]} text="Ipsum" timer={1.5} negative onClick={() => router.push("/ipsum")}/>
           <OrbitControls />
           <fog attach="fog" args={['white', 50, 190]} />
           <Environment preset="park" background  />
