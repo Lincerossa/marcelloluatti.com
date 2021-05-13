627a888bfaf33f2b76c6765d2cbf8f4a884d3065
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-13 11:52:47 +0200

    🔧 R3F: camera animation + OrbitControls disabled if the surfing is not done yet + mirrors surface loadedd with useGLFT + random lights position memoized

--- src/components/R3F/index.js
+++ src/components/R3F/index.js
@@ -1,7 +1,8 @@
 import React, {Suspense, useCallback, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, useGLTF, Text, Stars } from "@react-three/drei";
+import { MathUtils} from 'three'
+import { Canvas, useFrame,  } from '@react-three/fiber'
+import { OrbitControls, useGLTF, Text, Stars,  Plane} from "@react-three/drei";
 import * as S from './styles'
 import theme from '../../styles/theme';
 
@@ -21,12 +22,11 @@ const Model = (props) => {
   return (
     <group>
       <group ref={group} {...props} dispose={null} scale={0.4} position={props.position}>
-        <mesh>
-          <meshNormalMaterial  />
+        <mesh material={materials["Material.001"]}>
           <boxGeometry args={[1,2,1]} />
         </mesh>
       </group>
-      {isActive && <group {...props} dispose={null} position={[1, props.position[1], props.position[2]]} >
+      {isActive && <group {...props} dispose={null} position={[1, props.position[1], props.position[2]]}>
         <Text
           fontSize={.3}
           cursor="pointer"
@@ -43,21 +43,60 @@ const Model = (props) => {
     </group>
   )
 }
+
+
+const Lights = React.memo(() => {
+  function Rn(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  return (
+    <>
+    {Array.from({length: 20}).map((e,i) => {
+      return <pointLight key={i} intensity={Rn(0,5)} color={i% 2 ? theme.colors.primary : theme.colors.secondary} position={[Rn(-100, 100), Rn(-100, 100), Rn(-100, 100)]}  />
+    })}
+    <ambientLight intensity={500} color={theme.colors.secondary}  position={[0, 100, 100]}  />
+
+  </>)
+})
+
+function Surfing({setSurfed}) {
+  useFrame(({ clock, camera }) => {
+    if(camera.position.z > 5) {
+      camera.position.z = MathUtils.lerp(camera.position.z, 4, 0.06)
+    }
+    if(camera.position.y > 0) {
+      camera.position.y = MathUtils.lerp(camera.position.y, -1, 0.06)
+    }
+
+    if( (camera.position.y <= 0) && (camera.position.z <= 5)) {
+      camera.position.y = 0
+      camera.position.z = 5
+      setSurfed(true)
+    }
+    
+  })
+  return null
+}
+
+
 const R3F = () => {
   const router = useRouter()
-  
+  const [surfed, setSurfed] = useState(null)
 
   return (
     <S.CanvasWrapper>
-      <Canvas>
+      <Canvas camera={{ fov: 75, position: [0, 5, 700] }}>
         <Suspense fallback={null}>
-          <OrbitControls />
+          {surfed && <OrbitControls />}
           <Stars />
-          <Model position={[0, 2, 0]} text="About" timer={.5} negative onClick={() => router.push("/about")} />
-          <Model position={[0, 1, 0]} text="Projects" timer={.75} onClick={() => router.push("/projects")} />
-          <Model position={[0, 0, 0]} text="Blog" timer={1} negative onClick={() => router.push("/blog")}/>
-          <Model position={[0, -1, 0]} text="Lorem" timer={1.25} onClick={() => router.push("/lorem")}/>
-          <Model position={[0, -2, 0]} text="Ipsum" timer={1.5} negative onClick={() => router.push("/ipsum")}/>
+          {!surfed && <Surfing setSurfed={setSurfed} />}
+          <Model position={[0, 2, 0]} text="About" timer={1} negative onClick={() => router.push("/about")} />
+          <Model position={[0, 1, 0]} text="Projects" timer={1.25} onClick={() => router.push("/projects")} />
+          <Model position={[0, 0, 0]} text="Blog" timer={1.5} negative onClick={() => router.push("/blog")}/>
+          <Model position={[0, -1, 0]} text="Lorem" timer={1.75} onClick={() => router.push("/lorem")}/>
+          <Model position={[0, -2, 0]} text="Ipsum" timer={2} negative onClick={() => router.push("/ipsum")}/>
+          <Lights />
         </Suspense>
       </Canvas>
     </S.CanvasWrapper>
