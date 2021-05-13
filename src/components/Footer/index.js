b185d8664cf6fd4a88740fb56ef23f2186c5db54
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-13 23:49:09 +0200

    🚀 footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import * as S from './styles'
 import Wrapper from '../Wrapper'
+import Link from 'next/link'
+import * as C from './../../styles/common'
 
 const Footer = (props) => (
   <S.Footer>
     <Wrapper size="large">
       <S.FooterInner>
-        footer
+        <S.Logo>
+          <Link href="/">
+            <a>
+              <C.Glitch text="MLua">MLua</C.Glitch>
+            </a>
+          </Link>
+        </S.Logo>
+        <S.ExternalLinks>
+          <a href="https://github.com/Lincerossa" target="_blank">Github</a>
+          <a href="https://twitter.com/cav_lince" target="_blank">Twitter</a>
+          <a href="https://www.linkedin.com/in/marcello-luatti/" target="_blank">Linkedin</a>
+        </S.ExternalLinks>
       </S.FooterInner>
     </Wrapper>
   </S.Footer>
