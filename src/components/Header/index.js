f77a6004c9e7a33af864f05412dce1a8dfb4b5e6
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-14 11:16:32 +0200

    ✏️ as prop Link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -41,7 +41,7 @@ const Header = ({ routes, route, showMenu }) => {
                 routes?.filter(e => !e.hidden).map(({slug, label}) => {
                   return(
                     <S.MenuItem key={slug} isActive={slug === route?.slug} inverted={shouldBeInverted}>
-                      <Link href={`/${slug}`}>
+                      <Link href={`/${slug}`} as={`/${slug}`}>
                         <a>{label}</a>
                       </Link>
                     </S.MenuItem>
