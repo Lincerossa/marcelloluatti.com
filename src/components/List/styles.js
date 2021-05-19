506d9a4f063e55b9a7163674856a5b505592470f
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-19 10:38:26 +0200

    🐞 List: the number of columns were accidentally hard coded

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,15 +1,12 @@
 import styled from 'styled-components'
 
-export const List = styled.div.attrs({
-  className: 'List',
-})`
+export const List = styled.div`
   width: 100%;
   display: grid;
   grid-gap: 2rem;
   @media (min-width: 978px){
-    grid-template-columns: repeat(3,1fr);
+    grid-template-columns: ${(props) => `repeat(${props.columns}, 1fr)`};
   }
-  
 `
 
 export const ListItem = styled.div`
