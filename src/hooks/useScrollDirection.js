9ec1cf3d2b2880b9a903d595876576d4363af4be
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-20 11:08:07 +0200

    🦄 useScrollDirection eslinted + refactor (small)

--- src/hooks/useScrollDirection.js
+++ src/hooks/useScrollDirection.js
@@ -1,27 +1,32 @@
-import  { useState, useEffect} from 'react'
-import usePrevious from './usePrevious'
+import { useState, useEffect, useRef } from 'react'
 
-function useScrollDirection () {
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
+function useScrollDirection() {
   const [scrollPosition, setScrollPosition] = useState(null)
   const prevScrollPosition = usePrevious(scrollPosition)
-  
-  function handleGetDirection(){
+
+  function handleGetDirection() {
     setScrollPosition(window.scrollY)
   }
 
   useEffect(() => {
-    if(typeof window === "undefined") return
+    if (typeof window === 'undefined') return
     window.addEventListener('scroll', handleGetDirection, false)
     return () => {
       window.removeEventListener('scroll', handleGetDirection, false)
     }
   }, [])
 
-
   return ({
-    scrollDirection: (scrollPosition > prevScrollPosition) && (scrollPosition > 100) ? "down" : "up",
-    scrollPosition
+    scrollDirection: (scrollPosition > prevScrollPosition) && (scrollPosition > 100) ? 'down' : 'up',
+    scrollPosition,
   })
 }
 export default useScrollDirection
