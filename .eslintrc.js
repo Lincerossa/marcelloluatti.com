bf7ff241f371dd1be4a78c9d296e93d1f298fdb8
marcelloluatti <marcello.luatti@cloudacademy.com>
2021-11-22 09:31:42 +0100

    feat: 🎸 eslint throw now an error any time to any semi

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
     'no-use-before-define': 'off',
-    semi: 0,
+    semi: ['error', 'never'],
     'import/extensions': 0,
     '@typescript-eslint/no-use-before-define': ['error'],
     'import/prefer-default-export': 0,
