87f609cdd210743b11980065134260c18ac7bc99
marcelloluatti <marcello.luatti@cloudacademy.com>
2021-11-22 12:33:34 +0100

    refactor: 💡 ListOfCards moved to ts with enhancing the Card

---
+++ src/components/ListOfCards/index.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { FC } from 'react'
+import Link from 'next/link'
+
+import Card from '../Card'
+import List from '../List'
+import * as S from './styles'
+
+import { TListOfCardsProps } from './types'
+
+const ListOfCards: FC<TListOfCardsProps> = ({ items }) => (
+  <List columns={3}>
+    {items.map(({
+      slug, image, supertitle, title, tags, subtitle, layout,
+    }) => (
+      <Link key={slug} href="/[...dynamic]" as={`/${slug}`}>
+        <a>
+          <S.ListItem>
+            <Card
+              image={image}
+              supertitle={supertitle}
+              title={title}
+              subtitle={subtitle}
+              tags={tags}
+              layout={layout}
+            />
+          </S.ListItem>
+        </a>
+      </Link>
+    ))}
+  </List>
+)
+
+export default ListOfCards
